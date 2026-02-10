6d88084754f7fa1886e7ca05a2fe27a964e4db23
Abhishek Raj <ffabhishek116@gmail.com>
2026-02-10 20:57:43 +0530

    Add files via upload

---
+++ login_system.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+#include "json.hpp";
+using namespace std;
+using json = nlohmann::json;
+
+
+int main(void){
+    string userName;
+    int userPassword;
+
+    cout<<"Enter the username: ";
+    cin>>userName;
+
+    cout<<"Enter the Password: ";
+    cin>>userPassword;
+
+    // if(userName)
+
+
+    return 0;
+}
