bb23a8cf2485f856cd38a4b02293c2aca8694f4e
ffabhishek116@gmail.com <ffabhishek116@gmail.com>
2026-02-24 05:23:49 -0800

    add new function for user Experience

---
+++ index.cpp
@@ -0,0 +1,228 @@
+// #include <iostream>
+// #include <vector>
+// #include <map>
+// using namespace std;
+
+// map that store the key value pairs
+
+// int main(void){
+//     map <string , int> names;
+//     names["5"] =89;
+//     names["2"] = 78;
+//     names["3"] = 87;
+//     names["4"] = 67;
+
+//     for(auto it : names){
+//         cout<<it.first<<":"<<it.second<<endl;
+//     }
+// }
+
+// map methods--->
+
+// int main(void)
+// {
+
+    // store the value
+
+    // map<string, int> names;
+    // map<string, int> names1;
+
+    // names["Abhishek"] = 21;
+    // names["Abhishek"] = 22;
+    // auto value = names.begin();
+    // names.clear();
+    // cout<<names.size()<<endl;
+    // auto value = names.count("Abhishek");
+    // auto value = names.crbegin();
+    // cout<<value<<endl;
+    // auto value = names.emplace("Abhishek");
+    // cout<<value<<endl;
+    // auto value = names.empty();
+    // cout<<value <<endl;
+    // auto value = names.end();
+    // cout<<value<<endl;
+    // auto value = names.value_comp();
+    // cout<<value<<endl;
+    // names.erase("Abhishek");
+    // auto value = names.find("Abhishek");
+    // cout<<value<<endl;
+    // names.insert({"Ankush",22});
+    // auto value = names.lower_bound("Abhishek");
+    // cout<<value<<endl;
+    // auto value = names.max_size();
+    // cout<<value<<endl;
+
+    // if(names.empty()!=1){
+    //     for (auto name : names){
+    //         cout<<name.first <<" : "<<name.second<<endl;
+    //     }
+    // }else{
+    //     cout<<"No Data Found!"<<endl;
+    // }
+
+// #include <iostream>
+// #include <map>
+// #include <string>
+
+//     using namespace std;
+
+//     class Student
+//     {
+//     public:
+//         string name;
+//         int age;
+
+//         Student() {}
+//         Student(string n, int a)
+//         {
+//             name = n;
+//             age = a;
+//         }
+
+//         void display()
+//         {
+//             cout << "\nName: " << name;
+//             cout << "\nAge: " << age << endl;
+//         }
+//     };
+
+//     class StudentManagementSystem
+//     {
+
+//     private:
+//         map<int, Student> students;
+
+//     public:
+//         void add_student()
+//         {
+//             int id, age;
+//             string name;
+
+//             cout << "Enter ID: ";
+//             cin >> id;
+
+//             cout << "Enter Name: ";
+//             cin >> name;
+
+//             cout << "Enter Age: ";
+//             cin >> age;
+
+//             students[id] = Student(name, age);
+//         }
+
+//         void display_students()
+//         {
+//             for (auto &s : students)
+//             {
+//                 cout << "\nID: " << s.first;
+//                 s.second.display();
+//                 cout << "------------------";
+//             }
+//         }
+//     };
+
+//     int main()
+//     {
+
+//         StudentManagementSystem system;
+
+//         system.add_student();
+//         system.display_students();
+
+//         return 0;
+//     }
+
+// #include<iostream>
+// using namespace std;
+
+
+// class Animal{
+//     protected:
+//         string name = "Abhishek Raj";
+//         int age = 20;
+//     string greeting(){
+//         return "Congratulation";
+//     }    
+//     public:
+//         void greet(){
+//             cout<<greeting()<<endl;
+//         }
+//         Animal(){
+//             cout<<"this is Animal Constructor"<<endl;
+//         }
+//         void bark(string name){
+//             cout<<name<<" is Barking"<<endl;
+//         }
+// };
+// //single inheritance
+// class Dog{
+//     public:
+//         Dog(){
+//             cout<<"Dog Constructor"<<endl;
+//         }   
+//         //dog class eating function
+//         void eat(string name){
+//             cout<<name<<" is Eating..."<<endl;
+//         }
+// };
+// class Cat : public Animal,public Dog{
+//     public:
+//         Cat(){
+//             cout<<"cat Constructor..."<<endl;
+//         }
+// };
+
+// int swap_numbers(int,int);
+
+// int main(){
+    
+    // Cat *c = new Cat;
+    // c->eat("hello");
+    // c->bark("hello");
+
+    // string names[4] = {"Abhishek Raj","Ankush Raj","arun Chauhan","Brajwasi Singh"};
+    // for(int i=0;i<=names->length(); i++){
+    //     cout<<i<<" : "<<names[i]<<endl;
+    // }
+    // for(string &n : names){
+    //     cout<<n<<endl;
+    // }
+    // swap_numbers(2,4);
+// }
+//function prototype
+// int swap_numbers(int a,int b){
+//     cout<<"a: "<<a<<" b: "<<b<<endl;
+//     int demo = a;
+//     a= b;
+//     b= demo;
+//     cout<<"a: "<<a<<" b: "<<b<<endl;
+// }
+ //friend function ------>
+
+ #include<iostream>
+ using namespace std;
+
+class Student{
+    private:
+        string names = "Abhishek Raj";
+        int salarys = 200000;
+        void greeting(){
+            cout<<"welcome "<<names<<endl;
+        }
+
+    public:
+        Student(string name,int salary){
+            names = name;
+            salarys= salary;
+        }
+        friend void getdetails(Student &s){
+            
+        }
+
+
+};
+
+int main(void){
+    Student s("Abhishek",209090);
+    displayDetails(s);
+}
--- main.cpp
+++ main.cpp
@@ -1,129 +1,260 @@
 #include<iostream>
-#include<vector>
+#include<map>
 #include<string>
-#include<fstream>
-#include "json.hpp"
+#include<windows.h>
+#include<vector>
+
 using namespace std;
-using json = nlohmann::json;
-
-class StudentResultManagementSystem{
-
-public: 
-
-vector <string> subjectNames = {"Math","English","physics","Chemistry","Hindi"};
-vector <string> studentsNames;
-vector <int> studentAges;
-vector <int> marks;
 
 
-StudentResultManagementSystem (){
-    ifstream file("data.json");
-}
-
-void savedData(){
-    ofstream file("data.json", ios::app);
-    // file>>
-}
-
-// void 
-
-void AddStudentDetails(){
-    string name;
-    int age;
-
-    cout<<"Enter te Student Name: ";
-    cin>>name;
-
-    cout<<"Enter the "<<name<<" age:";
-    cin>>age;
-    
-    if(name.empty()|| age<=0){
-        cout<<"Invalid input"<<endl;
-    } else {
-        // studentsNames.push_back(name);
-        // studentAges.push_back(age);
-        cout<<"hello"<<endl;
-
-    }
-    
-}
-
-void AddSubjectMarks(){
-    int mark;
-    for (int i =0; i<subjectNames.size(); i++){
-        cout<<subjectNames[i] <<" Marks: ";
-        try{
-            cin>>mark;
-            if(mark>100){
-                throw "number can't be Higher than 100";
-            } else if (mark<0){
-                throw "Number can't be less than Zero";
-            } else {
-                 marks.push_back(mark);
-            }
-
-        } catch (const string* var){
-            cout<<var<<endl;
-        }
-    }
-}
-
-void displayDetails(){
-
-    int totalMarks =0;
-    int avg = 0;
-    int percentage = 0;
-    for (int mark : marks){
-        totalMarks+=mark;
-    }
-    avg = totalMarks / marks.size();
-    
-    percentage = ( avg * 100 ) / 500;
-
-    if (percentage >=90){
-        cout<<"Grade A"<<endl;
-    } else if (90>percentage<=70){
-        cout<<"Grade B"<<endl;
-    } else if(70>percentage<=50) {
-        cout<<"Grade C"<<endl;
-    } else if (60>percentage <= 33){
-        cout<<"Grade D"<<endl;
-    } else {
-        cout<<"Fail"<<endl;
-    }
-
-    cout<<"Student marks: "<<endl;
-    for(int j =0; j<subjectNames.size(); j++){
-        cout<<subjectNames[j]<<" : "<<marks[j]<<endl;
-    }
-}
-
-
+class StudentManagementSystem {
+    public:
+    struct user_details{
+        string student_name;
+        int student_age;
+        int password;
+        map<string, float> subject_marks;
+    };
+    map<int,user_details> student;
+        
+  
 };
 
-int main(void) {
-    StudentResultManagementSystem s;
-    bool isRunning = true;
-    int option;
-
-    while(isRunning){
-        cout<<"1. Add Student \n2. Add Marks \n3. Display marks \n4. Exit"<<endl;
-        cout<<"Enter the option: ";
-        cin>>option;
-
-        if (option == 1){
-            s.AddStudentDetails();
-        } else if(option == 2) {
-            s.AddSubjectMarks();
-        } else if(option == 3){
-            s.displayDetails();
-        } else if(option == 4){
-            cout<<"Data Saved Succesfully"<<endl;
-            break;
-        } else {
-            cout<<"Invalid Option"<<endl;
+class Student : public StudentManagementSystem{
+    private:
+        map <string,int> teacherPanel = {
+            {"Abhishek", 1234}
+        };
+        
+    public:
+    user_details user;
+    //add student details
+    void add_student_detail(int id,string name, int age,int password){
+        user.student_name = name;
+        user.student_age = age;
+        user.password = password;
+        student[id] = user;
+    }
+    //delete student 
+    void delete_student(string name,int id){
+        string student_name = name;
+        int student_id = id;
+        for(auto std : student){
+            Sleep(2000);
+            if(std.first==student_id && std.second.student_name==student_name){
+                student.erase(student_id);
+                cout<<"Deleted Sucessfully"<<endl;
+            }else{
+                cout<<"Not Founded"<<endl;
+            }
         }
     }
+    //add student marks
+    void add_student_marks(){
+        string subjects[5] = {"Hindi","English","Mathematics","Chemistry","Physics"};
+        float subject_marks;
+        for(string subject : subjects){
+            cout<<"Enter "<<subject<<" Marks: ";
+            cin>>subject_marks;
+            user.subject_marks[subject] = subject_marks;
+        }
+        cout<<"Added Succesfully"<<endl;
+    }
+    //get student marks
+    void get_student_marks(){
+        for(auto marks : user.subject_marks){
+            cout<<marks.first<<" : "<<marks.second<<endl;
+        }
+    }
+    //geting the student details ig: Name,Age,ID
+    void studentProfile(){
+        for(auto name : student){
+            cout<<"Id: "<<name.first <<"\nName: "<<name.second.student_name<<"\nAge: "<<name.second.student_age<<endl;
+        }
+    }
+    //for searching the student
+    void search_student(string name, int id){
+        for(auto std : student){
+            cout<<"Searching"<<endl;
+            Sleep(2000);
+            if(std.second.student_name==name && std.first==id){
+                cout<<"Founded"<<endl;
+            }else{
+                cout<<"Not Founded"<<endl;
+            }
+        }
+    }
+    //teacher portal for check the teacher is real or not
+    void teacherPortal(){
+        
+        string teacherName;
+        int password;
+        cout<<"Welcome to teacher panel: "<<endl;
+        cout<<"Enter the Username: ";
+        cin>>teacherName;
+        cout<<"Enter the password: ";
+        cin>>password;
+        for(auto teacher : teacherPanel){
+            if(teacher.first == teacherName && teacher.second== password){
+                Sleep(2000);
+                cout<<"Login Sucessfully"<<endl;
+                controlsForTeacher();
+            }
+            else{
+                cout<<"Something went wrong"<<endl;
+                teacherPortal();
+            }
+        }
+    };
+    //control panel for teacher where the teacher can Add,Delete,search,Add Marks,
+    void controlsForTeacher(){
+        bool is_running = true;
+        while(is_running){
+            int userOption;
+            //also add a section where teacher can update the marks (UpdateMarks)
+            cout<<"1.Add_student\n2.Delete Student\n3.Search Student\n4.Add Marks\n5.Menu\n6.Exits"<<endl;
+            cout<<"Enter the Option: ";
+            cin>>userOption;
 
+            if(userOption == 1){
+                string name;
+                int age,id,password;
+                cout<<"Enter Student ID: ";
+                cin>>id;
+                cout<<"Enter Student Name: ";
+                cin>>name;
+                cout<<"Enter "<<name<<"'s Age: ";
+                cin>>age;
+                cout<<"Enter password: ";
+                cin>>password;
+                add_student_detail(id,name,age,password);
+                cout<<"Done ✔"<<endl;
+            }else if(userOption == 2){
+                string name;
+                int id;
+                cout<<"Enter the Student ID: ";
+                cin>>id;
+                cout<<"Enter Student Name: ";
+                cin>>name;
+                delete_student(name,id);
+            }else if(userOption ==3){
+                string name;
+                int id;
+                cout<<"Enter Student Name: ";
+                cin>>name;
+                cout<<"Enter Student ID: ";
+                cin>>id;
+                search_student(name,id);
+            }
+            else if(userOption==4){
+                add_student_marks();
+            }
+            else if(userOption==5){
+                menu();
+            }
+            else if(userOption==6){
+                cout<<"Exiting..."<<endl;
+                Sleep(2000);
+                cout<<"Exit Succesfully"<<endl;
+                break;
+            }else{
+                cout<<"Invalid Option!"<<endl;
+            }
+        }
+    }
+    //student portal where student can login and check his details 
+    void studentPortal(){
+        int id,password;
+        bool is_running= true;
+        cout<<"Welcome to Student Poratl\n"<<endl;
+        while(is_running){
+            int userOption;
+            cout<<"1.Login\n2.Exit"<<endl;
+            cout<<"Enter the Option: ";
+            cin>>userOption;
+            if(userOption==1){
+                cout<<"Enter your ID: ";
+                cin>>id;
+                cout<<"Enter you password(D-O-B): ";
+                cin>>password;
+                for(auto it: student){
+                    if(id==it.first && password==it.second.password){
+                        controlsForStudent();
+                    }else{
+                        cout<<"Contact to Teacher"<<endl;
+                        break;
+                    }
+                }
+            }else if(userOption==2){
+                Sleep(2000);
+                cout<<"Exited"<<endl;
+                break;
+            }
+        }
+    }
+    void controlsForStudent(){
+        int userOption;
+        bool is_running = true;
+        while(is_running){
+            cout<<"1.Profile\n2.Result\n3.Update Password\n4.Menu\n5.Exit"<<endl;
+            cout<<"Enter the option: ";
+            cin>>userOption;
+            if(userOption==1){
+                studentProfile();
+            } else if(userOption==2){
+                get_student_marks();
+            } else if(userOption==3){
+                int password,id,newPassword;
+                cout<<"Enter ID: ";
+                cin>>id;
+                cout<<"Enter the old password: ";
+                cin>>password;
+                for(auto &user: student){
+                    if(id==user.first && password== user.second.password){
+                        cout<<"Enter the New Password: ";
+                        cin>>newPassword;
+                        user.second.password=newPassword;
+                        cout<<"Updated"<<endl;
+                    }
+                }
+            }else if(userOption==4){
+                menu();
+            }else if(userOption==5){
+                Sleep(2000);
+                cout<<"Exited"<<endl;
+            }else{
+                cout<<"Invalid Option"<<endl;
+            }
+        }
+    }
+//menu section
+    void menu(){
+        bool is_running = true;
+        int userOption;
+        cout<<"1.Teacher Panel\n2.Student Panel\n3.Exits"<<endl;
+        cout<<"Enter the Option: ";
+        cin>>userOption;
+        switch (userOption){
+            case 1:
+                teacherPortal();
+                break;
+            case 2:
+                studentPortal();
+                break;       
+            case 3:
+                Sleep(2000);
+                cout<<"Exited"<<endl;
+                break;
+            default :
+                cout<<"Invalid Option"<<endl;
+            }
+        }
+};
+int main(void){
+    Student std;
+    std.menu();
+    
     return 0;
 }
+
+
